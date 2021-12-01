f929f4dd1377625ffe1c0698e8763c7ff71a3a5e
Rasmus R. Friis <rasmus@friiz.dk>
2021-12-01 22:34:15 +0100

    server updateret

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 // modules =================================================
 const express = require('express');
 const app = express();
+const path = require("path");
 // set our port
 const port = 3100;
-app.get('/userlist', (req, res) => res.send('Welcome to køogsalg!'));
-app.post('/users', (req, res) => res.send('Welcome to køogsalg!'));
-app.get('/', (req, res) => res.send('Welcome to køogsalg!'));
-app.get('/', (req, res) => res.send('Welcome to køogsalg!'));
+const userlistpath = "./model/users.json";
+app.get('/users', (req, res) => {
+    res.sendFile(path.join(__dirname, "./model/users.json"))
+    
+    //res.send('Welcome to købogsalg!')
+});
 
+const itemspath = "./model/items.json"
+app.get('/items', (req, res) => {
+    res.sendFile(path.join(__dirname, "./model/items.json"))
+
+});
+
+const itemtypepath = "./model/itemtype.json"
+app.get('/items', (req, res) => {
+    res.sendFile(path.join(__dirname, "./model/itemtype.json"))
+
+});
+
+const indexpath = "./index.html"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"))
+
+});
 // startup our app at http://localhost:3100
 app.listen(port, () => console.log(`Server are listening on ${port}!`));
