cae3f4dbdf7515e6a8756bb57584346fe6f09fdc
Rasmus R. Friis <rasmus@friiz.dk>
2021-12-02 22:58:59 +0100

    modifier server

--- server/server.js
+++ server/server.js
@@ -9,50 +9,58 @@ const fs = require('fs');
 const port = 3100;
 
 const userlistpath = "./model/users.json";
+const itemspath = "./model/items.json"
+const itemtypepath = "./model/itemtype.json"
+const indexpath = "./index.html"
+
 app.get('/users', (req, res) => {
     res.sendFile(path.join(__dirname, userlistpath))
 
-    app.post('/users', (req, res) => {
+});
+
+app.post('/users', (req, res) => {
         res.send("POST users Request Called")
       })
 
       console.log("ramte denne users path");
     
-});
 
-const itemspath = "./model/items.json"
 app.get('/items', (req, res) => {
     res.sendFile(path.join(__dirname, itemspath))
 
-    app.post('/items', (req, res) => {
+});
+
+ app.post('/items', (req, res) => {
         res.send("POST items Request Called")
       })
 
       console.log("ramte denne items path");
 
-});
 
-const itemtypepath = "./model/itemtype.json"
 app.get('/itemtype', (req, res) => {
     res.sendFile(path.join(__dirname, itemtypepath))
 
-    app.post('/itemtype', (req, res) => {
+    
+});
+
+app.post('/itemtype', (req, res) => {
         res.send("POST itemtype Request Called")
       })
 
       console.log("ramte denne itemtype path");
-});
 
-const indexpath = "./index.html"
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, indexpath))
 
-    app.post('/', (req, res) => {
+   
+
+});
+
+ app.post('/', (req, res) => {
         res.send("POST Request Called")
       })
 
       console.log("ramte denne path");
 
-});
 // startup our app at http://localhost:3100
 app.listen(port, () => console.log(`Server are listening on ${port}!`));
