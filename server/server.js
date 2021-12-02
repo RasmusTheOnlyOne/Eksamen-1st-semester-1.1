5a9d33a4b0698cbd9f7c88d88b9c4e166e82ce4c
Rasmus R. Friis <rasmus@friiz.dk>
2021-12-02 22:35:53 +0100

    app.post tilfojet

--- server/server.js
+++ server/server.js
@@ -7,28 +7,51 @@ const fs = require('fs');
 
 // set our port
 const port = 3100;
+
 const userlistpath = "./model/users.json";
 app.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname, "./model/users.json"))
+    res.sendFile(path.join(__dirname, userlistpath))
+
+    app.post('/users', (req, res) => {
+        res.send("POST users Request Called")
+      })
+
+      console.log("ramte denne users path");
     
-    //res.send('Welcome to købogsalg!')
 });
 
 const itemspath = "./model/items.json"
 app.get('/items', (req, res) => {
-    res.sendFile(path.join(__dirname, "./model/items.json"))
+    res.sendFile(path.join(__dirname, itemspath))
+
+    app.post('/items', (req, res) => {
+        res.send("POST items Request Called")
+      })
+
+      console.log("ramte denne items path");
 
 });
 
 const itemtypepath = "./model/itemtype.json"
 app.get('/itemtype', (req, res) => {
-    res.sendFile(path.join(__dirname, "./model/itemtype.json"))
+    res.sendFile(path.join(__dirname, itemtypepath))
 
+    app.post('/itemtype', (req, res) => {
+        res.send("POST itemtype Request Called")
+      })
+
+      console.log("ramte denne itemtype path");
 });
 
 const indexpath = "./index.html"
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./index.html"))
+    res.sendFile(path.join(__dirname, indexpath))
+
+    app.post('/', (req, res) => {
+        res.send("POST Request Called")
+      })
+
+      console.log("ramte denne path");
 
 });
 // startup our app at http://localhost:3100
