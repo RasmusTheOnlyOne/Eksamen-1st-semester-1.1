aed3484cfe7c3aff2091e372b3ed76d70d6c726c
Rasmus R. Friis <rasmus@friiz.dk>
2021-12-02 23:37:47 +0100

    body parse tilfojet

--- server/server.js
+++ server/server.js
@@ -15,44 +15,47 @@ const indexpath = "./index.html";
 
 app.get('/users', (req, res) => {
     res.sendFile(path.join(__dirname, userlistpath))
-    console.log("ramte denne users path");
+    console.log("ramte denne GET users path");
 });
 
 app.post('/users', (req, res) => {
-    res.send("POST users Request Called")
-    console.log("ramte denne users path");
+    console.log("ramte denne POST users path");
+    console.log("POST - /login - User logging in");
+    var user1 = req.body;
+    user1.id = "1";
+    console.log('Got body:', req.body);
+    console.log('A ', user1);
+    console.log("user: ", user1.name);
+    console.log('password: ', user1.password);
+    res.send(user1);
 })
 
 app.get('/items', (req, res) => {
     res.sendFile(path.join(__dirname, itemspath))
-    console.log("ramte denne items path");
+    console.log("ramte denne GET items path");
 });
 
 app.post('/items', (req, res) => {
-    res.send("POST items Request Called")
-    console.log("ramte denne items path");
+    console.log("ramte denne POST items path");
 })
 
 app.get('/itemtype', (req, res) => {
     res.sendFile(path.join(__dirname, itemtypepath))
-    console.log("ramte denne itemtype path");
+    console.log("ramte denne GET itemtype path");
 });
 
 app.post('/itemtype', (req, res) => {
-    res.send("POST itemtype Request Called")
-    console.log("ramte denne itemtype path");
+    console.log("ramte denne POST itemtype path");
 })
-
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, indexpath))
-    console.log("ramte denne path");
+    console.log("ramte denne GET index path");
 });
 
 app.post('/', (req, res) => {
-    res.send("POST Request Called")
-    console.log("ramte denne path");
+    console.log("ramte denne POST index path");
+
 })
 
 
